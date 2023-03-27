f1647766d149818a07d38c3c597c2fadac6e3d99
ymaltsman <jmaltsman@hmc.edu>
2023-03-27 17:01:33 -0500

    faulty merging

--- src/ofApp.h
+++ src/ofApp.h
@@ -29,15 +29,21 @@ class ofApp : public ofBaseApp{
 		void MakeEVEN(unsigned char* arr);
 		void flyp(unsigned char* arr1, unsigned char* arr2);
 		void constructTable();
+		void cluster();
+		bool avalanche();
+		void avalancheGO(int idx);
+		bool eqcolors(char arr1[3], char arr2[3]);
+		void operatesandpile();
 
 		void chooseT(); //choose transition function
-		void addT(); //additive transition
+		void addT(int a); //additive transition
 		void mult5(); //multiplicative transition
 		void sqrdiff();
 
 		//update functions
 		void add(char params[3]);
 		void mult(char params[3]);
+		void niceMatrix(char params[3][3]);
 		void Matmul(char params[3][3]);
 		void signswitch();
 		void rotate();
@@ -50,6 +56,7 @@ class ofApp : public ofBaseApp{
 
 	bool trans_ON; //set transition
 	bool pp; // alter images for multiplicative transition
+	bool faulty_merge;
 	int max_diff; //tracks difference between current and next image
 	int trans; //for choosing transition function
 	int col_idx; //track index of pixel in image
@@ -64,4 +71,6 @@ class ofApp : public ofBaseApp{
 	int steps[256][256];//table for multiplicative order
 	int mousex, mousey; //mouseclick
 	int m; //for lemma14()
+	string MODE;
+	char clusters[5][3];
 };
